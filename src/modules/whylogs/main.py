2f6368e24022513ce60255c9c5e47496e4c890d8
Jinglebear <jinglebearbeowulf@gmail.com>
2022-12-22 19:55:57 +0100

    update main

--- src/modules/whylogs/main.py
+++ src/modules/whylogs/main.py
@@ -4,19 +4,18 @@ from wilds.common.data_loaders import get_train_loader
 import torchvision.transforms as transforms
 from wilds.datasets.wilds_dataset import WILDSSubset
 from progressbar import progressbar
-# Load the full dataset, and download it if necessary
-dataset = get_dataset(dataset="camelyon17", download=False)
-CAMELYON_ROOT_PATH ='/home/jinglewsl/evoila/sandbox/whylogs_v1/ml-image-drift-monitoring/src/modules/whylogs/data'
 from whylogs_logger import whylogs_logger
 w_logger = whylogs_logger()
 import random
 from PIL.Image import Image 
 import pandas as pd
 import numpy as np
+import os
+CAMELYON_ROOT_PATH = '{}/data'.format(os.getcwd())
+CSV_PATH = '{}/data/whylogs_output/profile_compare/'.format(os.getcwd())
 def main():
-   
 
-
+    
     """ WILDS CAMELYON DATASET """
     dataset = get_dataset(dataset="camelyon17", download=False)
 
@@ -25,13 +24,14 @@ def main():
 
     train_data = dataset.get_subset("train")
 
+
     # Get the validation set (in distribution)
 
-    val_data = dataset.get_subset("val")
+    # val_data = dataset.get_subset("val")
 
     # Get the test set (out of distribution)
 
-    test_data = dataset.get_subset("test")
+    # test_data = dataset.get_subset("test")
 
 
     """ LOG DATASETS TO PROFILES AND SERIALIZE TO BINARIES"""
@@ -40,40 +40,70 @@ def main():
     # pil_images_val = [ val_data.dataset[idx][0] for idx in progressbar(val_data.indices)]
     # pil_images_test =  [ test_data.dataset[idx][0] for idx in progressbar(test_data.indices)]
 
+
     
     # size = int(len(train_data.indices))
-    for i in range(20,100,5):
-        if i == 25:
-            continue
+    for i in range(80,100,5):
         log_percentage_of_dataset(percentage=i,pil_images=pil_images_train,size=int(len(train_data.indices)),split='train')
         # log_percentage_of_dataset(percentage=i,pil_images=pil_images_val,size=int(len(val_data.indices)),split='val')
         # log_percentage_of_dataset(percentage=i,pil_images=pil_images_test,size=int(len(test_data.indices)),split='test')
 
 
     """ COMPARE DISTRIBUTIONS """
+    # import pandas as pd
+    # run train vs train comp
+    # for i in range(5,60,5):
+    #     if i == 25:
+    #         continue
+    #     train_profile_1 =  w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('train',i))
+    #     train_profile_2 =  w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('train',100))
+    #     log_compare(target=train_profile_1,ref=train_profile_2,data_directory_path=CAMELYON_ROOT_PATH,compare_summary_name='train_{}_vs_train_{}'.format(i,100))
+
+    # dfs =[]
+    # for j in range(5,60,5):
+    #     if j == 25:
+    #         continue
+    #     dfs += read_csv(csv_path=CSV_PATH,csv_name='train_{}_vs_train_100'.format(j),percentage=j)
+        
+    
+
+    # df_joined = dfs[0]
+    # for i in range(1,10,1):
+    #     df_joined = df_joined.join(dfs[i].set_index('metric'),on='metric')
+
+    # df_joined.to_csv('tmp.csv')
+
+    
+    
+    # converted_df = pd.DataFrame(data)
+
+    # print(converted_df)
+
+    # plot_difference(dfs=dfs,output_path=CSV_PATH,target='test',target_p=5,ref='train',ref_p=100)   
+    
 
     # run train vs test comp --> .csv
-    # for i in range(5,100,5):
-        # for j in range(5,100,5):
-            # test_profile =  w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('test',j))
-            # train_profile = w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('train',i))
-            # log_compare(target=test_profile,ref=train_profile,data_directory_path=CAMELYON_ROOT_PATH,compare_summary_name='train_{}_vs_test_{}'.format(i,j))
+    # for i in range(5,55,5):
+    #     for j in range(5,105,5):
+    #         test_profile =  w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('test',j))
+    #         train_profile = w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('train',i))
+    #         log_compare(target=test_profile,ref=train_profile,data_directory_path=CAMELYON_ROOT_PATH,compare_summary_name='train_{}_vs_test_{}'.format(i,j))
     
     # run train vs val comp --> .csv
-    # for i in range(5,100,5):
-    #     for j in range(5,100,5):
+    # for i in range(5,105,5):
+    #     for j in range(5,105,5):
     #         val_profile =  w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('val',j))
     #         train_profile = w_logger.deserialize_profile(data_directory_path=CAMELYON_ROOT_PATH,binary_name='{}_camelyon_profile_{}'.format('train',i))
     #         log_compare(target=val_profile,ref=train_profile,data_directory_path=CAMELYON_ROOT_PATH,compare_summary_name='train_{}_vs_val_{}'.format(i,j))
  
     """ PLOT DIFFERENCE test x vs train 95"""
-    CSV_PATH = '/home/jinglewsl/evoila/sandbox/whylogs_v1/ml-image-drift-monitoring/src/modules/whylogs/data/whylogs_output/profile_compare/'
-    OUT = '/home/jinglewsl/evoila/sandbox/whylogs_v1/ml-image-drift-monitoring/src/modules/whylogs/data/whylogs_output/profile_compare/'
-    dfs = []
-    for i in range(5,100,5):
-        dfs += read_csv(csv_path=CSV_PATH,csv_name='train_95_vs_test_{}'.format(i))
-    for i in range(5,100,5):
-        plot_difference(percentage=i,dfs=dfs,output_path=OUT,target='test',ref='train',ref_p=95)
+    # CSV_PATH = '/home/jinglewsl/evoila/sandbox/whylogs_v1/ml-image-drift-monitoring/src/modules/whylogs/data/whylogs_output/profile_compare/'
+    # OUT = '/home/jinglewsl/evoila/sandbox/whylogs_v1/ml-image-drift-monitoring/src/modules/whylogs/data/whylogs_output/profile_compare/'
+    # dfs = []
+    # for i in range(5,100,5):
+    #     dfs += read_csv(csv_path=CSV_PATH,csv_name='train_95_vs_test_{}'.format(i))
+    # for i in range(5,100,5):
+    #     plot_difference(percentage=i,dfs=dfs,output_path=OUT,target='test',ref='train',ref_p=95)
     """ CREATE VIZ FROM COMPARE """
     
     # w_logger.create_visualization(data_directory_path=CAMELYON_ROOT_PATH,viz_name='25_train_camelyon_v_train_camelyon',target_profile=train_camelyon_profile_25,referece_profile=train_camelyon_profile)
@@ -84,12 +114,12 @@ def read_csv(csv_path :str, csv_name : str, percentage : int) -> pd.DataFrame:
     df =  [pd.read_csv('{}{}.csv'.format(csv_path,csv_name)).drop("Unnamed: 0",axis=1).drop('rating',axis=1).rename(columns={'p_val':'{}'.format(percentage)})]
     return df
 
-def plot_difference(percentage : int, dfs : List(pd.DataFrame),output_path:str, target: str,ref:str, ref_p :int):
+def plot_difference(dfs : List[pd.DataFrame],output_path:str, target: str,target_p : int, ref:str, ref_p :int):
     df_joined = dfs[0]
     for i in range(1,20,1):
         df_joined = df_joined.join(dfs[i].set_index('metric'),on='metric')
     data= {
-    'metric' :           [ str(out) for out in range(5,100,5)],
+    'metric' :           [ str(out) for out in range(5,105,5)],
     'Brightness.mean':   df_joined[df_joined['metric'] == 'image.Brightness.mean' ].to_numpy()[0][1:],
     'Brightness.stddev' :df_joined[df_joined['metric'] == 'image.Brightness.stddev' ].to_numpy()[0][1:],
     'Hue.mean' :         df_joined[df_joined['metric'] == 'image.Hue.mean' ].to_numpy()[0][1:],
@@ -98,23 +128,26 @@ def plot_difference(percentage : int, dfs : List(pd.DataFrame),output_path:str, 
     'ImagePixelWidth':   df_joined[df_joined['metric'] == 'image.ImagePixelWidth' ].to_numpy()[0][1:],
     'Saturation.mean':   df_joined[df_joined['metric'] == 'image.Saturation.mean' ].to_numpy()[0][1:],
     'Saturation.stddev': df_joined[df_joined['metric'] == 'image.Saturation.stddev' ].to_numpy()[0][1:],
-    'Drift Threshold' :  np.full(19,0.05),
+    'Drift Threshold' :  np.full(20,0.05),
     }
+    
+    
     converted_df = pd.DataFrame(data)
     my_yticks = np.arange(0,1,0.05)
-
-    ax =converted_df.plot.line(
+    print(converted_df)
+    ax =converted_df.plot(
+        kind='line',
         y=['Brightness.mean', 'Brightness.stddev', 'Hue.mean','Hue.stddev','ImagePixelHeight','ImagePixelWidth','Saturation.mean','Saturation.stddev'],
         x='metric',
         figsize=(12,6), 
-        title='{} vs {} {}'.format(ref,target, percentage),
+        title='{}{} vs {} {}'.format(target,target_p,ref,ref_p),
         xlabel='Batch size percentage',
-        yticks=my_yticks,
+        yticks=my_yticks
         )
     converted_df.plot(kind='line',y='Drift Threshold',color='black',ax=ax)
 
 
-    plt.savefig( '{}{}_{}_v_{}_{}.png'.format(output_path,ref,ref_p,target,percentage))
+    plt.savefig( '{}{}_{}_v_{}_{}.png'.format(output_path,target,target_p,ref,ref_p))
 
 
 def log_compare(target,ref,data_directory_path,compare_summary_name):
@@ -127,7 +160,7 @@ def log_compare(target,ref,data_directory_path,compare_summary_name):
         compare_summary_name=compare_summary_name)
 
 
-    w_logger.create_drift_metric_df_from_comp_summary_json(data_directory_path=data_directory_path,compare_summary_name=compare_summary_name)
+    print(w_logger.create_drift_metric_df_from_comp_summary_json(data_directory_path=data_directory_path,compare_summary_name=compare_summary_name))
 
 
 
@@ -141,7 +174,7 @@ def log_percentage_of_dataset(percentage: int, pil_images : List[Image], size : 
         data_directory_path=CAMELYON_ROOT_PATH,
         pil_data_arr=pil_images[:count],
     )
-    w_logger.serialize_profile(profile=tmp_profile,binary_name='{}_camelyon_profile_{}'.format(split,percentage),data_directory_path=CAMELYON_ROOT_PATH)
+    print(w_logger.serialize_profile(profile=tmp_profile,binary_name='{}_camelyon_profile_{}'.format(split,percentage),data_directory_path=CAMELYON_ROOT_PATH))
 
 
 # ======================================================================================
