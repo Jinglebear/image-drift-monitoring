51a47c2a5efd33c5b779f140175235fc1f3ec95f
Jinglebear <jinglebearbeowulf@gmail.com>
2022-12-31 17:29:12 +0000

    update(main): whylogs main, added multiprocessing

--- src/modules/whylogs/main.py
+++ src/modules/whylogs/main.py
@@ -1,29 +1,40 @@
-from typing import List
-from wilds import get_dataset
-from wilds.common.data_loaders import get_train_loader
-import torchvision.transforms as transforms
-from wilds.datasets.wilds_dataset import WILDSSubset
-from progressbar import progressbar
-from whylogs_logger import whylogs_logger
-w_logger = whylogs_logger()
-import random
-from PIL.Image import Image 
-import pandas as pd
-import numpy as np
 import os
-CAMELYON_ROOT_PATH = '{}/data'.format(os.getcwd())
-CSV_PATH = '{}/data/whylogs_output/profile_compare/'.format(os.getcwd())
-def main():
+import random
+from multiprocessing import Process, Queue
+from timeit import default_timer as timer
+from typing import List
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from whylogs_logger import Whylogs_Logger
+from wilds import get_dataset
+from whylogs.core.view.dataset_profile_view import DatasetProfileView
+
+def main():
+    CAMELYON_ROOT_PATH = '{}/data'.format(os.getcwd())
+    GLOBALWHEAT_ROOT_PATH = '{}/data/global_wheat_v1.1'.format(os.getcwd())
+    IWILDCAM_ROOT_PATH = '{}/data/iwildcam_v2.0'.format(os.getcwd())
+    CSV_PATH = '{}/data/whylogs_output/profile_compare/'.format(os.getcwd())
+
+    
+    w_logger = Whylogs_Logger() # init whylogs logger 
+    
+    t = timer() # setup timer
     
     """ WILDS CAMELYON DATASET """
     # dataset = get_dataset(dataset="camelyon17", download=True)
+    """ WILDS GLOBALWHEAT DATASET """
+    # dataset = get_dataset(dataset="globalwheat", download=False)
+    """ WILDS IWILDCAM DATASET """
+    dataset = get_dataset(dataset="iwildcam", download=False)
+    """ WILDS POVERTY DATASET """
+    # dataset = get_dataset(dataset="poverty",download=True)
 
-    """ GET DATASETS """
+    """ GET SPLITS """
     # Get the training set (in distribution)
 
-    # train_data = dataset.get_subset("train")
-
+    train_data = dataset.get_subset("train")
 
     # Get the validation set (in distribution)
 
@@ -34,21 +45,144 @@ def main():
     # test_data = dataset.get_subset("test")
 
 
-    """ LOG DATASETS TO PROFILES AND SERIALIZE TO BINARIES"""
 
-    # pil_images_train = [ train_data.dataset[idx][0] for idx in progressbar(train_data.indices)]
-    # pil_images_val = [ val_data.dataset[idx][0] for idx in progressbar(val_data.indices)]
-    # pil_images_test =  [ test_data.dataset[idx][0] for idx in progressbar(test_data.indices)]
-
-
+    """ LOG DATASET TO BINARY USING MULTIPROCESSING"""
+    log_profile_to_bin_multiple_processes(
+        w_logger=w_logger,
+        percentage=100,
+        indices=train_data.indices,
+        dataset=train_data.dataset,
+        num_processes=6,
+        split='train',
+        dataset_name='iwildcam',
+        dataset_dir_path=IWILDCAM_ROOT_PATH)
     
-    # size = int(len(train_data.indices))
-    for i in range(80,100,5):
-        log_percentage_of_dataset(percentage=i,pil_images=pil_images_train,size=int(len(train_data.indices)),split='train')
-        # log_percentage_of_dataset(percentage=i,pil_images=pil_images_val,size=int(len(val_data.indices)),split='val')
-        # log_percentage_of_dataset(percentage=i,pil_images=pil_images_test,size=int(len(test_data.indices)),split='test')
+
+    dt = timer() - t
+    print(f'Time (s) {dt:.3f}')
+
+# logging split (x) from dataset (y) with percentage (z) to binary x_y_profile_z.bin
+def log_profile_to_bin_multiple_processes(w_logger : Whylogs_Logger, percentage: int, indices : List[int], dataset: any, num_processes: int, split: str, dataset_name: str, dataset_dir_path : str):
+    
+    count = int(len(indices) * (percentage / 100)) # calculate logging amout from percentage
+
+    indices_arr = indices # get the indices to array
+
+    random.shuffle(indices_arr) # shuffle array in place 
+
+    indices_arr = indices_arr[:count]
+    split_array = np.array_split(indices_arr,num_processes) # split array into x arrays for x processes
+    
+    
+    queue = Queue() # create queue to gather process output
+    profiles = [] # create array to store profiles
+    processes = [] # array to check running processes
+
+    # define processes with task and args
+    for i in range(num_processes):
+        tmp_process = Process(target=logging_task,args=(split_array[i],queue,dataset,w_logger))
+        processes.append(tmp_process)
+
+    # start processes
+    for p in processes:
+        p.start()
+
+    # prevent deadlock for queue
+    while 1:
+        running = any(p.is_alive() for p in processes)
+        while not queue.empty():
+            profiles.append(queue.get())
+        if not running:
+            break
+    
+    # wait for all processes to finish
+    for p in processes:
+        p.join()
 
 
+    # debug print 
+    for p in profiles:
+        print(p)
+    
+    
+    merged_profiles = w_logger.merge_profiles(profiles=profiles) # merge all profiles from processes
+    print('merged Profiles')
+
+    print(w_logger.serialize_profile(profile=merged_profiles,binary_name='{}_{}_profile_{}'.format(split,dataset_name,percentage),data_directory_path=dataset_dir_path)) # serialize the merged profiles to output bin
+
+   
+
+# needed for logging (multiprocess)
+def logging_task(indices : any,queue : Queue, dataset: any,w_logger : Whylogs_Logger):
+    first_profile = w_logger.log_pil_image_to_profile(image=dataset[indices[0]][0],profile=None)
+
+    for idx in indices[1:]:
+        first_profile = w_logger.log_pil_image_to_profile(image=dataset[idx][0],profile=first_profile)
+
+    queue.put(first_profile)
+    print('process finished task')
+
+
+# load dataframe from comparison csv
+def read_csv(csv_path :str, csv_name : str, percentage : int) -> pd.DataFrame:
+    df =  [pd.read_csv('{}{}.csv'.format(csv_path,csv_name)).drop("Unnamed: 0",axis=1).drop('rating',axis=1).rename(columns={'p_val':'{}'.format(percentage)})]
+    return df
+
+
+
+# needed ?
+def plot_difference(dfs : List[pd.DataFrame],output_path:str, target: str,target_p : int, ref:str, ref_p :int):
+    df_joined = dfs[0]
+    for i in range(1,20,1):
+        df_joined = df_joined.join(dfs[i].set_index('metric'),on='metric')
+    data= {
+    'metric' :           [ str(out) for out in range(5,105,5)],
+    'Brightness.mean':   df_joined[df_joined['metric'] == 'image.Brightness.mean' ].to_numpy()[0][1:],
+    'Brightness.stddev' :df_joined[df_joined['metric'] == 'image.Brightness.stddev' ].to_numpy()[0][1:],
+    'Hue.mean' :         df_joined[df_joined['metric'] == 'image.Hue.mean' ].to_numpy()[0][1:],
+    'Hue.stddev' :       df_joined[df_joined['metric'] == 'image.Hue.stddev' ].to_numpy()[0][1:],
+    'ImagePixelHeight':  df_joined[df_joined['metric'] == 'image.ImagePixelHeight' ].to_numpy()[0][1:],
+    'ImagePixelWidth':   df_joined[df_joined['metric'] == 'image.ImagePixelWidth' ].to_numpy()[0][1:],
+    'Saturation.mean':   df_joined[df_joined['metric'] == 'image.Saturation.mean' ].to_numpy()[0][1:],
+    'Saturation.stddev': df_joined[df_joined['metric'] == 'image.Saturation.stddev' ].to_numpy()[0][1:],
+    'Drift Threshold' :  np.full(20,0.05),
+    }
+    
+    
+    converted_df = pd.DataFrame(data)
+    my_yticks = np.arange(0,1,0.05)
+    print(converted_df)
+    ax =converted_df.plot(
+        kind='line',
+        y=['Brightness.mean', 'Brightness.stddev', 'Hue.mean','Hue.stddev','ImagePixelHeight','ImagePixelWidth','Saturation.mean','Saturation.stddev'],
+        x='metric',
+        figsize=(12,6), 
+        title='{}{} vs {} {}'.format(target,target_p,ref,ref_p),
+        xlabel='Batch size percentage',
+        yticks=my_yticks
+        )
+    converted_df.plot(kind='line',y='Drift Threshold',color='black',ax=ax)
+
+
+    plt.savefig( '{}{}_{}_v_{}_{}.png'.format(output_path,target,target_p,ref,ref_p))
+
+# load binaries to profiles & create drift metric to compare
+def create_profile_comparison(w_logger : Whylogs_Logger, target_binary_name: str,ref_binary_name : str,data_directory_path :str,compare_summary_name: str):
+    
+    target = w_logger.deserialize_profile(data_directory_path=data_directory_path,binary_name=target_binary_name)
+    ref = w_logger.deserialize_profile(data_directory_path=data_directory_path,binary_name=ref_binary_name)
+
+    w_logger.create_profile_compare_summary_json(
+        target_profile=target,
+        ref_profile=ref,
+        data_directory_path=data_directory_path,
+        compare_summary_name=compare_summary_name)
+
+
+    print(w_logger.create_drift_metric_df_from_comp_summary_json(data_directory_path=data_directory_path,compare_summary_name=compare_summary_name))
+
+
+def misc():
     """ COMPARE DISTRIBUTIONS """
     # import pandas as pd
     # run train vs train comp
@@ -108,74 +242,6 @@ def main():
     
     # w_logger.create_visualization(data_directory_path=CAMELYON_ROOT_PATH,viz_name='25_train_camelyon_v_train_camelyon',target_profile=train_camelyon_profile_25,referece_profile=train_camelyon_profile)
     # w_logger.create_visualization(data_directory_path=CAMELYON_ROOT_PATH,viz_name='first_25_train_camelyon_v_next_25_train_camelyon',target_profile=train_camelyon_profile_next_25,referece_profile=train_camelyon_profile_25)
-
-import matplotlib.pyplot as plt
-def read_csv(csv_path :str, csv_name : str, percentage : int) -> pd.DataFrame:
-    df =  [pd.read_csv('{}{}.csv'.format(csv_path,csv_name)).drop("Unnamed: 0",axis=1).drop('rating',axis=1).rename(columns={'p_val':'{}'.format(percentage)})]
-    return df
-
-def plot_difference(dfs : List[pd.DataFrame],output_path:str, target: str,target_p : int, ref:str, ref_p :int):
-    df_joined = dfs[0]
-    for i in range(1,20,1):
-        df_joined = df_joined.join(dfs[i].set_index('metric'),on='metric')
-    data= {
-    'metric' :           [ str(out) for out in range(5,105,5)],
-    'Brightness.mean':   df_joined[df_joined['metric'] == 'image.Brightness.mean' ].to_numpy()[0][1:],
-    'Brightness.stddev' :df_joined[df_joined['metric'] == 'image.Brightness.stddev' ].to_numpy()[0][1:],
-    'Hue.mean' :         df_joined[df_joined['metric'] == 'image.Hue.mean' ].to_numpy()[0][1:],
-    'Hue.stddev' :       df_joined[df_joined['metric'] == 'image.Hue.stddev' ].to_numpy()[0][1:],
-    'ImagePixelHeight':  df_joined[df_joined['metric'] == 'image.ImagePixelHeight' ].to_numpy()[0][1:],
-    'ImagePixelWidth':   df_joined[df_joined['metric'] == 'image.ImagePixelWidth' ].to_numpy()[0][1:],
-    'Saturation.mean':   df_joined[df_joined['metric'] == 'image.Saturation.mean' ].to_numpy()[0][1:],
-    'Saturation.stddev': df_joined[df_joined['metric'] == 'image.Saturation.stddev' ].to_numpy()[0][1:],
-    'Drift Threshold' :  np.full(20,0.05),
-    }
-    
-    
-    converted_df = pd.DataFrame(data)
-    my_yticks = np.arange(0,1,0.05)
-    print(converted_df)
-    ax =converted_df.plot(
-        kind='line',
-        y=['Brightness.mean', 'Brightness.stddev', 'Hue.mean','Hue.stddev','ImagePixelHeight','ImagePixelWidth','Saturation.mean','Saturation.stddev'],
-        x='metric',
-        figsize=(12,6), 
-        title='{}{} vs {} {}'.format(target,target_p,ref,ref_p),
-        xlabel='Batch size percentage',
-        yticks=my_yticks
-        )
-    converted_df.plot(kind='line',y='Drift Threshold',color='black',ax=ax)
-
-
-    plt.savefig( '{}{}_{}_v_{}_{}.png'.format(output_path,target,target_p,ref,ref_p))
-
-
-def log_compare(target,ref,data_directory_path,compare_summary_name):
-       
-
-    w_logger.create_profile_compare_summary_json(
-        target_profile=target,
-        ref_profile=ref,
-        data_directory_path=data_directory_path,
-        compare_summary_name=compare_summary_name)
-
-
-    print(w_logger.create_drift_metric_df_from_comp_summary_json(data_directory_path=data_directory_path,compare_summary_name=compare_summary_name))
-
-
-
-
-def log_percentage_of_dataset(percentage: int, pil_images : List[Image], size : int, split: str):
-   
-    random.shuffle(pil_images)
-
-    count = int(size * (percentage / 100))
-    tmp_profile= w_logger.log_pil_images_data_from_list(
-        data_directory_path=CAMELYON_ROOT_PATH,
-        pil_data_arr=pil_images[:count],
-    )
-    print(w_logger.serialize_profile(profile=tmp_profile,binary_name='{}_camelyon_profile_{}'.format(split,percentage),data_directory_path=CAMELYON_ROOT_PATH))
-
 
 # ======================================================================================
 # call
