a3a0b130fddce5c3a73571a7d81478f7ce51a33f
Jinglebear <jinglebearbeowulf@gmail.com>
2023-02-15 19:44:32 +0000

    update

--- src/modules/alibi_detect/test/run_night4.py
+++ src/modules/alibi_detect/test/run_night4.py
@@ -25,13 +25,13 @@ def main():
         "40" : ["{}".format(i) for i in range(1,21,1)],
         "20" : ["{}".format(i) for i in range(1,21,1)],
         }
-        df_new = pd.DataFrame(data,index=["{}% OOD Bilder".format(i) for i in range(10,210,10)])
+        df_new = pd.DataFrame(data,index=["{}% OOD Bilder".format(i) for i in range(5,105,5)])
 
 
         for j in range(20,120,20):
                 myUAE = UntrainedAutoencoder()
                 myUAE.import_detector(path='/home/ubuntu/image-drift-monitoring/config/detectors/GlobalWheat/UAE/KS/globalwheat_UAE_{}_KS'.format(j),detector_type='KS')
-                for i in range(10,210,10):
+                for i in range(5,105,5):
                         test_i_comp = np.load('/home/ubuntu/image-drift-monitoring/data/globalwheat_v1.1/drifted_data/incremental_drift/globalwheat_test_incremental_{}.npz'.format(i))
                         test_i = test_i_comp['arr_0']
                         res = myUAE.make_prediction(target_data=test_i, detector_type='KS')
@@ -41,7 +41,7 @@ def main():
         for j in range(20,120,20):
                 myUAE = UntrainedAutoencoder()
                 myUAE.import_detector(path='/home/ubuntu/image-drift-monitoring/config/detectors/GlobalWheat/UAE/CVM/globalwheat_UAE_{}_CVM'.format(j),detector_type='CVM')
-                for i in range(10,210,10):
+                for i in range(5,105,5):
                         test_i_comp = np.load('/home/ubuntu/image-drift-monitoring/data/globalwheat_v1.1/drifted_data/incremental_drift/globalwheat_test_incremental_{}.npz'.format(i))
                         test_i = test_i_comp['arr_0']
                         res = myUAE.make_prediction(target_data=test_i, detector_type='CVM')
@@ -51,7 +51,7 @@ def main():
         for j in range(20,120,20):
                 myUAE = UntrainedAutoencoder()
                 myUAE.import_detector(path='/home/ubuntu/image-drift-monitoring/config/detectors/GlobalWheat/UAE/MMD/globalwheat_UAE_{}_MMD'.format(j),detector_type='MMD')
-                for i in range(10,210,10):
+                for i in range(5,105,5):
                         test_i_comp = np.load('/home/ubuntu/image-drift-monitoring/data/globalwheat_v1.1/drifted_data/incremental_drift/globalwheat_test_incremental_{}.npz'.format(i))
                         test_i = test_i_comp['arr_0']
                         res = myUAE.make_prediction(target_data=test_i, detector_type='MMD')
@@ -61,7 +61,7 @@ def main():
         for j in range(20,120,20):
                 myUAE = UntrainedAutoencoder()
                 myUAE.import_detector(path='/home/ubuntu/image-drift-monitoring/config/detectors/GlobalWheat/UAE/LSDD/globalwheat_UAE_{}_LSDD'.format(j),detector_type='LSDD')
-                for i in range(10,210,10):
+                for i in range(5,105,5):
                         test_i_comp = np.load('/home/ubuntu/image-drift-monitoring/data/globalwheat_v1.1/drifted_data/incremental_drift/globalwheat_test_incremental_{}.npz'.format(i))
                         test_i = test_i_comp['arr_0']
                         res = myUAE.make_prediction(target_data=test_i, detector_type='LSDD')
