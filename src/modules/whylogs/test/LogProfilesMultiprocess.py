739bfd8ded4647407444904918b9e941963d0107
Jinglebear <jinglebearbeowulf@gmail.com>
2023-02-21 14:36:31 +0000

    update

--- src/modules/whylogs/test/LogProfilesMultiprocess.py
+++ src/modules/whylogs/test/LogProfilesMultiprocess.py
@@ -1,3 +1,4 @@
+from src.modules.whylogs.whylogs_logger import Whylogs_Logger
 import os
 import random
 from multiprocessing import Process, Queue
@@ -10,44 +11,38 @@ import pandas as pd
 from wilds import get_dataset
 from whylogs.core.view.dataset_profile_view import DatasetProfileView
 
-import torchvision.transforms as T
-import torch
-import torchvision
 import PIL
 
 import sys
 sys.path.append('/home/ubuntu/image-drift-monitoring')
-from src.modules.whylogs.whylogs_logger import Whylogs_Logger
+
+
 def main():
 
     CAMELYON_ROOT_PATH = '{}/data/camelyon17_v1.0'.format(os.getcwd())
     GLOBALWHEAT_ROOT_PATH = '{}/data/globalwheat_v1.1'.format(os.getcwd())
     IWILDCAM_ROOT_PATH = '{}/data/iwildcam_v2.0'.format(os.getcwd())
-    POVERTY_ROOT_PATH =  '{}/data/poverty_v1.1'.format(os.getcwd())
+    POVERTY_ROOT_PATH = '{}/data/poverty_v1.1'.format(os.getcwd())
     RXRX1_ROOT_PATH = '/home/ubuntu/image-drift-monitoring/data/rxrx1_v1.0'
     CSV_PATH = '{}/data/whylogs_output/profile_compare/'.format(os.getcwd())
 
-    
-    w_logger = Whylogs_Logger() # init whylogs logger 
-    
-    t = timer() # setup timer
-    
+    w_logger = Whylogs_Logger()  # init whylogs logger
+
+    t = timer()  # setup timer
+
     """ WILDS CAMELYON DATASET """
-    # dataset = get_dataset(dataset="camelyon17", download=False)
+    dataset = get_dataset(dataset="camelyon17", download=False)
     """ WILDS GLOBALWHEAT DATASET """
     # dataset = get_dataset(dataset="globalwheat", download=False)
     """ WILDS IWILDCAM DATASET """
     # dataset = get_dataset(dataset="iwildcam", download=False)
     """ WILDS RXRX1 DATASET """
     # dataset = get_dataset(dataset="rxrx1",download=False)
-    
-
-
 
     """ GET SPLITS """
     # Get the training set (in distribution)
 
-    # train_data = dataset.get_subset("train")
+    train_data = dataset.get_subset("train")
 
     # Get the validation set (in distribution)
 
@@ -57,23 +52,20 @@ def main():
 
     # test_data = dataset.get_subset("test")
 
-
-    
-
     """ LOG TRAIN SPLIT TO BINARY USING MULTIPROCESSING"""
-    # for i in range(5,105,5):    
-    #     log_profile_to_bin_multiple_processes(
-    #         w_logger=w_logger,
-    #         percentage=i,
-    #         indices=train_data.indices,
-    #         dataset=train_data.dataset,
-    #         num_processes=30,
-    #         split='train',
-    #         dataset_name='rxrx1',
-    #         dataset_dir_path=RXRX1_ROOT_PATH)
+    for i in range(5, 105, 5):
+        log_profile_to_bin_multiple_processes(
+            w_logger=w_logger,
+            percentage=i,
+            indices=train_data.indices,
+            dataset=train_data.dataset,
+            num_processes=30,
+            split='train',
+            dataset_name='rxrx1',
+            dataset_dir_path=RXRX1_ROOT_PATH)
 
     """ LOG VAL SPLIT TO BINARY USING MULTIPROCESSING"""
-    # for j in range(5,105,5):    
+    # for j in range(5,105,5):
     #     log_profile_to_bin_multiple_processes(
     #         w_logger=w_logger,
     #         percentage=j,
@@ -83,9 +75,9 @@ def main():
     #         split='val',
     #         dataset_name='rxrx1',
     #         dataset_dir_path=RXRX1_ROOT_PATH)
-    
+
     """ LOG TEST SPLIT TO BINARY USING MULTIPROCESSING"""
-    # for k in range(5,105,5):    
+    # for k in range(5,105,5):
     #     log_profile_to_bin_multiple_processes(
     #         w_logger=w_logger,
     #         percentage=k,
@@ -100,27 +92,28 @@ def main():
     print(f'Time (s) {dt:.3f}')
 
 
-
 # logging split (x) from dataset (y) with percentage (z) to binary x_y_profile_z.bin
-def log_profile_to_bin_multiple_processes(w_logger : Whylogs_Logger, percentage: int, indices : List[int], dataset: any, num_processes: int, split: str, dataset_name: str, dataset_dir_path : str):
-    
-    count = int(len(indices) * (percentage / 100)) # calculate logging amout from percentage
+def log_profile_to_bin_multiple_processes(w_logger: Whylogs_Logger, percentage: int, indices: List[int], dataset: any, num_processes: int, split: str, dataset_name: str, dataset_dir_path: str):
 
-    indices_arr = indices # get the indices to array
+    # calculate logging amout from percentage
+    count = int(len(indices) * (percentage / 100))
 
-    random.shuffle(indices_arr) # shuffle array in place 
+    indices_arr = indices  # get the indices to array
+
+    random.shuffle(indices_arr)  # shuffle array in place
 
     indices_arr = indices_arr[:count]
-    split_array = np.array_split(indices_arr,num_processes) # split array into x arrays for x processes
-    
-    
-    queue = Queue() # create queue to gather process output
-    profiles = [] # create array to store profiles
-    processes = [] # array to check running processes
+    # split array into x arrays for x processes
+    split_array = np.array_split(indices_arr, num_processes)
+
+    queue = Queue()  # create queue to gather process output
+    profiles = []  # create array to store profiles
+    processes = []  # array to check running processes
 
     # define processes with task and args
     for i in range(num_processes):
-        tmp_process = Process(target=logging_task,args=(split_array[i],queue,dataset,w_logger))
+        tmp_process = Process(target=logging_task, args=(
+            split_array[i], queue, dataset, w_logger))
         processes.append(tmp_process)
 
     # start processes
@@ -134,33 +127,35 @@ def log_profile_to_bin_multiple_processes(w_logger : Whylogs_Logger, percentage:
             profiles.append(queue.get())
         if not running:
             break
-    
+
     # wait for all processes to finish
     for p in processes:
         p.join()
 
-
-    # debug print 
+    # debug print
     for p in profiles:
         print(p)
-    
-    
-    merged_profiles = w_logger.merge_profiles(profiles=profiles) # merge all profiles from processes
+
+    merged_profiles = w_logger.merge_profiles(
+        profiles=profiles)  # merge all profiles from processes
     print('merged Profiles')
 
-    print(w_logger.serialize_profile(profile=merged_profiles,binary_name='{}_{}_profile_{}'.format(split,dataset_name,percentage),data_directory_path=dataset_dir_path)) # serialize the merged profiles to output bin
+    print(w_logger.serialize_profile(profile=merged_profiles, binary_name='{}_{}_profile_{}'.format(
+        split, dataset_name, percentage), data_directory_path=dataset_dir_path))  # serialize the merged profiles to output bin
 
-   
 
 # needed for logging (multiprocess)
-def logging_task(indices : any,queue : Queue, dataset: any,w_logger : Whylogs_Logger):
-    first_profile = w_logger.log_pil_image_to_profile(image=dataset[indices[0]][0],profile=None)
+def logging_task(indices: any, queue: Queue, dataset: any, w_logger: Whylogs_Logger):
+    first_profile = w_logger.log_pil_image_to_profile(
+        image=dataset[indices[0]][0], profile=None)
 
     for idx in indices[1:]:
-        first_profile = w_logger.log_pil_image_to_profile(image=dataset[idx][0],profile=first_profile)
+        first_profile = w_logger.log_pil_image_to_profile(
+            image=dataset[idx][0], profile=first_profile)
 
     queue.put(first_profile)
     print('process finished task')
+
 
 if __name__ == "__main__":
     main()
--- src/modules/whylogs/whylogs_logger.py
+++ src/modules/whylogs/whylogs_logger.py
@@ -1,4 +1,10 @@
 """ standard imports """
+from whylogs.viz.utils.profile_viz_calculations import (
+    generate_profile_summary, generate_summaries)
+from whylogs.viz import NotebookProfileVisualizer
+from whylogs.migration import uncompound
+from whylogs.extras.image_metric import log_image
+from whylogs.core.view.dataset_profile_view import DatasetProfileView
 import json
 import logging
 import os
@@ -15,17 +21,9 @@ from progressbar import progressbar
 
 """ whylogs imports  """
 
-from whylogs.core.view.dataset_profile_view import DatasetProfileView
-from whylogs.extras.image_metric import log_image
-from whylogs.migration import uncompound
-from whylogs.viz import NotebookProfileVisualizer
-from whylogs.viz.utils.profile_viz_calculations import (
-    generate_profile_summary, generate_summaries)
-
 
 class Whylogs_Logger():
-    def __init__(self, config = None) -> None:
-        
+    def __init__(self, config=None) -> None:
         """ init output paths """
         self.OUTPUT_PATH = '/whylogs_output'
         self.OUTPUT_BIN = '/whylogs_output/bins'
@@ -38,7 +36,8 @@ class Whylogs_Logger():
         self.my_datetime = datetime.now(pytz.timezone('Europe/Berlin'))
 
         """ init logger """
-        my_format = logging.Formatter('%(asctime)s [%(levelname)s]  %(message)s')
+        my_format = logging.Formatter(
+            '%(asctime)s [%(levelname)s]  %(message)s')
         handler = logging.StreamHandler()
         handler.setLevel(logging.DEBUG)
         handler.setFormatter(my_format)
@@ -46,20 +45,24 @@ class Whylogs_Logger():
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(handler)
 
-
     """ create output directories """
-    def create_output_dir(self,path :str) -> None:
-        pathlib.Path(path + self.OUTPUT_PATH).mkdir(parents=True,exist_ok=True)
-        pathlib.Path(path + self.OUTPUT_BIN).mkdir(parents=True,exist_ok=True)
-        pathlib.Path(path + self.PROFILE_SUMMARIES).mkdir(parents=True,exist_ok=True)
-        pathlib.Path(path + self.PROFILE_COMPARE).mkdir(parents=True,exist_ok=True)
 
+    def create_output_dir(self, path: str) -> None:
+        pathlib.Path(
+            path + self.OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(path + self.OUTPUT_BIN).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(
+            path + self.PROFILE_SUMMARIES).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(
+            path + self.PROFILE_COMPARE).mkdir(parents=True, exist_ok=True)
 
     """ log basic images from directory to a profile, set logging date """
-    def log_basic_image_data_from_dir(self, data_directory_path: str,sub_dir_path: str, batch_size: int = 0, shuffle: bool = False) -> DatasetProfileView:
+
+    def log_basic_image_data_from_dir(self, data_directory_path: str, sub_dir_path: str, batch_size: int = 0, shuffle: bool = False) -> DatasetProfileView:
         try:
-            self.create_output_dir(data_directory_path) # create output dirs 
-            self.logger.info('Started logging image data from: {}'.format(sub_dir_path))
+            self.create_output_dir(data_directory_path)  # create output dirs
+            self.logger.info(
+                'Started logging image data from: {}'.format(sub_dir_path))
             profile = None
             data_directory_list = os.listdir(sub_dir_path)
             if shuffle and batch_size > 0:
@@ -68,21 +71,25 @@ class Whylogs_Logger():
             for img_name in progressbar(data_directory_list):
                 img = Image.open(sub_dir_path + img_name)  # read in image
                 _ = log_image(img).profile()  # generate profile
-                _.set_dataset_timestamp(self.my_datetime) # optionally set dataset_timestamp
+                # optionally set dataset_timestamp
+                _.set_dataset_timestamp(self.my_datetime)
                 _view = _.view()  # extract mergeable profile view
                 if profile is None:
                     profile = _view
                 else:
-                    profile = profile.merge(_view) # merge each profile while looping
-            self.logger.info('Logged image data from: {} with batch size {}'.format(sub_dir_path, profile.to_pandas()['image/Brightness.mean:counts/n']['image'])) #  get batchsize from output data
+                    # merge each profile while looping
+                    profile = profile.merge(_view)
+            self.logger.info('Logged image data from: {} with batch size {}'.format(sub_dir_path, profile.to_pandas()[
+                             'image/Brightness.mean:counts/n']['image']))  # get batchsize from output data
             return profile
         except Exception as e:
             self.logger.exception('Exception in log_data(): {}'.format(e))
-    
+
     """ log pil images from a list to a profile, set logging date """
-    def log_pil_images_data_from_list(self,pil_data_arr: List[PIL_IMAGE],batch_size: int = 0, shuffle: bool = False) -> DatasetProfileView:
+
+    def log_pil_images_data_from_list(self, pil_data_arr: List[PIL_IMAGE], batch_size: int = 0, shuffle: bool = False) -> DatasetProfileView:
         try:
-            # self.create_output_dir(data_directory_path) # create output dirs 
+            # self.create_output_dir(data_directory_path) # create output dirs
             self.logger.info('Started logging PIL image data from array')
             profile = None
             if shuffle and batch_size > 0:
@@ -90,21 +97,25 @@ class Whylogs_Logger():
                 pil_data_arr = pil_data_arr[0:batch_size]
             for img in progressbar(pil_data_arr):
                 _ = log_image(img).profile()  # generate profile
-                _.set_dataset_timestamp(self.my_datetime) # optionally set dataset_timestamp
+                # optionally set dataset_timestamp
+                _.set_dataset_timestamp(self.my_datetime)
                 _view = _.view()  # extract mergeable profile view
                 if profile is None:
                     profile = _view
                 else:
-                    profile = profile.merge(_view) # merge each profile while looping
-            self.logger.info('Logged image data from PIL image data with batch size {}'.format(profile.to_pandas()['image/Brightness.mean:counts/n']['image'])) #  get batchsize from output data
+                    # merge each profile while looping
+                    profile = profile.merge(_view)
+            self.logger.info('Logged image data from PIL image data with batch size {}'.format(
+                profile.to_pandas()['image/Brightness.mean:counts/n']['image']))  # get batchsize from output data
             return profile
         except Exception as e:
             self.logger.exception('Exception in log_data(): {}'.format(e))
 
-    def log_pil_image_to_profile(self,image : PIL_IMAGE, profile : DatasetProfileView) -> DatasetProfileView:
+    def log_pil_image_to_profile(self, image: PIL_IMAGE, profile: DatasetProfileView) -> DatasetProfileView:
         try:
             _ = log_image(image).profile()
-            _.set_dataset_timestamp(self.my_datetime) # optionally set dataset_timestamp
+            # optionally set dataset_timestamp
+            _.set_dataset_timestamp(self.my_datetime)
             _view = _.view()
             if profile is None:
                 profile = _view
@@ -112,63 +123,75 @@ class Whylogs_Logger():
                 profile = profile.merge(_view)
             return profile
         except Exception as e:
-            self.logger.exception('Exception in log_pil_image_to_profile: {}'.format(e))
+            self.logger.exception(
+                'Exception in log_pil_image_to_profile: {}'.format(e))
 
-    def merge_profiles(self,profiles : List[DatasetProfileView]) -> DatasetProfileView:
-        first = profiles[0] 
+    def merge_profiles(self, profiles: List[DatasetProfileView]) -> DatasetProfileView:
+        first = profiles[0]
         for profile in profiles[1:]:
             first = first.merge(profile)
         return first
 
-
-
     """ serialize a profile to a .bin """
-    def serialize_profile(self,profile: DatasetProfileView, binary_name: str, data_directory_path:str) -> str:
+
+    def serialize_profile(self, profile: DatasetProfileView, binary_name: str, data_directory_path: str) -> str:
         try:
-            serialize_profile_bin_path = '{}{}/{}.bin'.format(data_directory_path,self.OUTPUT_BIN,binary_name)
+            serialize_profile_bin_path = '{}{}/{}.bin'.format(
+                data_directory_path, self.OUTPUT_BIN, binary_name)
             profile.write(serialize_profile_bin_path)
             return serialize_profile_bin_path
         except Exception as e:
-            self.logger.exception('Exception in serialize_profile(): {}'.format(e))
-
+            self.logger.exception(
+                'Exception in serialize_profile(): {}'.format(e))
 
     """ deserialize a profile from a .bin """
-    def deserialize_profile(self,data_directory_path: str, binary_name:str) -> DatasetProfileView:
-        try:
-            return DatasetProfileView.read('{}{}/{}.bin'.format(data_directory_path,self.OUTPUT_BIN,binary_name))
-        except Exception as e:
-            self.logger.exception('Exception in deserialize_profile(): {}'.format(e))
 
+    def deserialize_profile(self, data_directory_path: str, binary_name: str) -> DatasetProfileView:
+        try:
+            return DatasetProfileView.read('{}{}/{}.bin'.format(data_directory_path, self.OUTPUT_BIN, binary_name))
+        except Exception as e:
+            self.logger.exception(
+                'Exception in deserialize_profile(): {}'.format(e))
 
     """ create profile summary json"""
-    def create_profile_summary_json(self,profile: DatasetProfileView, data_directory_path: str, summary_name : str) -> str:
+
+    def create_profile_summary_json(self, profile: DatasetProfileView, data_directory_path: str, summary_name: str) -> str:
         try:
-            profile_summary_json_path = '{}{}/{}.json'.format(data_directory_path,self.PROFILE_SUMMARIES,summary_name)
+            profile_summary_json_path = '{}{}/{}.json'.format(
+                data_directory_path, self.PROFILE_SUMMARIES, summary_name)
             with open(profile_summary_json_path, 'w') as output_file:
-                output_file.write(str(generate_profile_summary(target_view=uncompound._uncompound_dataset_profile(profile), config=None)['profile_from_whylogs']))
-            self.logger.info('Created profile summary json for {}'.format(summary_name))
+                output_file.write(str(generate_profile_summary(
+                    target_view=uncompound._uncompound_dataset_profile(profile), config=None)['profile_from_whylogs']))
+            self.logger.info(
+                'Created profile summary json for {}'.format(summary_name))
             return profile_summary_json_path
         except Exception as e:
-            self.logger.exception('Exception in create_profile_summary(): {}'.format(e))
-
+            self.logger.exception(
+                'Exception in create_profile_summary(): {}'.format(e))
 
     """ create profile comparison json """
-    def create_profile_compare_summary_json(self,target_profile: DatasetProfileView, ref_profile: DatasetProfileView, data_directory_path: str, compare_summary_name: str) -> str:
+
+    def create_profile_compare_summary_json(self, target_profile: DatasetProfileView, ref_profile: DatasetProfileView, data_directory_path: str, compare_summary_name: str) -> str:
         try:
-            compare_summary_json_path = '{}{}/{}.json'.format(data_directory_path,self.PROFILE_COMPARE,compare_summary_name)
+            compare_summary_json_path = '{}{}/{}.json'.format(
+                data_directory_path, self.PROFILE_COMPARE, compare_summary_name)
             with open(compare_summary_json_path, 'w') as output_file:
-                target_uncompound = uncompound._uncompound_dataset_profile(target_profile)
-                ref_uncompound = uncompound._uncompound_dataset_profile(ref_profile)
-                output_file.write(str(generate_summaries(target_view=target_uncompound,ref_view=ref_uncompound, config=None)['reference_profile_from_whylogs']))
+                target_uncompound = uncompound._uncompound_dataset_profile(
+                    target_profile)
+                ref_uncompound = uncompound._uncompound_dataset_profile(
+                    ref_profile)
+                output_file.write(str(generate_summaries(target_view=target_uncompound,
+                                  ref_view=ref_uncompound, config=None)['reference_profile_from_whylogs']))
             return compare_summary_json_path
         except Exception as e:
-            self.logger.exception('Exception in create_profile_compare_summary_json(): {}'.format(e))
-
+            self.logger.exception(
+                'Exception in create_profile_compare_summary_json(): {}'.format(e))
 
     """ create metric and p_val dataframes """
-    def create_drift_metric_df_from_comp_summary_json(self,data_directory_path: str, compare_summary_name : str) -> pd.DataFrame:
+
+    def create_drift_metric_df_from_comp_summary_json(self, data_directory_path: str, compare_summary_name: str) -> pd.DataFrame:
         try:
-            with open('{}{}/{}.json'.format(data_directory_path,self.PROFILE_COMPARE,compare_summary_name), 'r') as input:
+            with open('{}{}/{}.json'.format(data_directory_path, self.PROFILE_COMPARE, compare_summary_name), 'r') as input:
                 data = json.load(input)
             tmp = {"metric": [], "p_val": [], "rating": []}
             for metric in data['columns']:
@@ -184,21 +207,26 @@ class Whylogs_Logger():
                         tmp['rating'].append('No Drift (0.15 - 1.00)')
             df = pd.DataFrame.from_dict(tmp)
             df.sort_values(by=['metric'], inplace=True)
-            df.to_csv('{}{}/{}.csv'.format(data_directory_path,self.PROFILE_COMPARE,compare_summary_name))
+            df.to_csv('{}{}/{}.csv'.format(data_directory_path,
+                      self.PROFILE_COMPARE, compare_summary_name))
             return df
         except Exception as e:
-            self.logger.exception('Exception in create_drift_metric_df_from_comp_summary_json(): {}'.format(e))
-
+            self.logger.exception(
+                'Exception in create_drift_metric_df_from_comp_summary_json(): {}'.format(e))
 
     """ create visualization  """
-    def create_visualization(self,data_directory_path:str, viz_name:str, target_profile: DatasetProfileView, referece_profile: DatasetProfileView) -> str:
+
+    def create_visualization(self, data_directory_path: str, viz_name: str, target_profile: DatasetProfileView, referece_profile: DatasetProfileView) -> str:
         try:
             _viz = NotebookProfileVisualizer()
-            _viz.set_profiles(target_profile_view=target_profile,reference_profile_view=referece_profile)
+            _viz.set_profiles(target_profile_view=target_profile,
+                              reference_profile_view=referece_profile)
             html_viz = _viz.summary_drift_report()
-            html_viz_path = '{}{}/{}.html'.format(data_directory_path,self.PROFILE_COMPARE,viz_name)
+            html_viz_path = '{}{}/{}.html'.format(
+                data_directory_path, self.PROFILE_COMPARE, viz_name)
             with open(html_viz_path, 'w') as output:
                 output.write(html_viz.data)
             return html_viz_path
         except Exception as e:
-            self.logger.exception('Exception in create_metrics(): {}'.format(e))
+            self.logger.exception(
+                'Exception in create_metrics(): {}'.format(e))
